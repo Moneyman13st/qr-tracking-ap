20407aeacb161ea1fef20514055d90064c26a589
Moneyman13st <Moneyman13st@gmail.com>
2025-02-09 13:14:06 -0500

    Fixed syntax error in app.py and synced with remote

--- generate_qr.py
+++ generate_qr.py
@@ -1,11 +1,11 @@
 import qrcode
 
 def create_qr():
-    url = "http://127.0.0.1:5000/"  # Change this when deploying online
+    url = "https://qr-tracking-ap-1.onrender.com"  # Change this when deploying online
     qr = qrcode.make(url)
     qr.save("qr_code.png")
     print("✅ QR Code successfully saved as 'qr_code.png'!")  # Added print confirmation
 
-create_qr()pip3 freeze > requirements.txt
+create_qr()
 
 
